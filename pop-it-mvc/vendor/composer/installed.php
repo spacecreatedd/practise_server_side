772fc52f28366d824b9a22ec99c125db45ac04bd
spacecreatedd <kimzhenya1313@mail.ru>
2024-03-13 16:46:07 +0700

    vendor and composer lock

---
+++ pop-it-mvc/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'spacecreatedd/practise_server_side',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '0cc1e150e4eaa88918bbe0b8627b022506ff441d',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'spacecreatedd/practise_server_side' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '0cc1e150e4eaa88918bbe0b8627b022506ff441d',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
